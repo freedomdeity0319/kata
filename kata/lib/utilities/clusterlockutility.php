8a38b0683504373ff60b77119919441630bc7dc1
liveatmeganite <liveatmeganite@538d6a13-39f8-77ab-848e-733ac6ff8734>
2011-08-02 14:33:10 +0000

    clusterlockutil: choose memcache or memcacheD

--- kata/lib/utilities/clusterlockutility.php
+++ kata/lib/utilities/clusterlockutility.php
@@ -1,10 +1,8 @@
 <?php
+
 /**
  * @package kata
  */
-
-
-
 
 /**
  * CLUSTERwide locking mechanism with timeout for critical sections or eventhandlers
@@ -14,42 +12,61 @@
  * @author feldkamp@gameforge.de
  */
 class ClusterlockUtility {
+
 	/**
 	 * @var integer seconds to wait until we timeout
 	 */
-	private $timeout= 10;
-
+	private $timeout = 10;
 	/**
 	 * @var array holds lock-status
 	 */
-	private $locks= array ();
-
+	private $locks = array();
 	/**
 	 * placeholder for cache-utility
 	 */
 	private $cacheUtil = null;
+	/**
+	 * placeholder for cache-utility
+	 */
+	private $forceType = false;
 
 	/**
 	 * @param integer $timout how many seconds to wait for a lock before we fail
 	 */
 	public function setTimeout($timeout) {
 		if (is_numeric($timeout) && ($timeout > 0)) {
-			$this->timeout= $timeout;
+			$this->timeout = $timeout;
 			return true;
 		}
 		return false;
 	}
 
 	/**
+	 * set which type of memcache-extension we should use
+	 * 
+	 * @param bool true=memcached.extension false=memcache.extension
+	 */
+	public function setCacheType($withD=false) {
+		$this->initialize();
+		$this->forceType = $withD ? CacheUtility::CM_MEMCACHED : CacheUtility::CM_MEMCACHE;
+	}
+
+	/**
 	 * initialize internal structures
- 	 */
-	protected function initialize() {
-		if (!defined('MEMCACHED_SERVERS') || (strlen(MEMCACHED_SERVERS)==0)) {
+	 */
+	protected function initialize($checkForType=false) {
+		if (!defined('MEMCACHED_SERVERS') || (strlen(MEMCACHED_SERVERS) == 0)) {
 			throw new RuntimeException('clusterlockutil: no memcached-servers defined in config');
 		}
 
 		if (null === $this->cacheUtil) {
 			$this->cacheUtil = getUtil('Cache');
+		}
+		
+		if ($checkForType) {
+			if (false === $this->forceType) {
+				throw new Exception('call setCacheType() first');
+			}
 		}
 	}
 
@@ -62,14 +79,14 @@ class ClusterlockUtility {
 	 * @uses CacheUtility::add
 	 */
 	function lock($id, $waitForTimeout= true) {
-		$this->initialize();
+		$this->initialize(true);
 
-		$timeout= time() + $this->timeout;
-		$lockId= CACHE_IDENTIFIER.'clusterLockUtilHandle'.urlencode($id);
+		$timeout = time() + $this->timeout;
+		$lockId = CACHE_IDENTIFIER . 'clusterLockUtilHandle' . urlencode($id);
 		$success = false;
 
 		while ((time() < $timeout) && $waitForTimeout) {
-			if ($this->cacheUtil->add($lockId,1,$this->timeout,CacheUtility::CM_MEMCACHE)) {
+			if ($this->cacheUtil->add($lockId, 1, $this->timeout, $this->forceType)) {
 				$success = true;
 				break;
 			}
@@ -77,7 +94,7 @@ class ClusterlockUtility {
 		}
 
 		if ($success) {
-			$this->locks[$id]= true;
+			$this->locks[$id] = true;
 			return true;
 		}
 
@@ -94,21 +111,21 @@ class ClusterlockUtility {
 	function unlock($id) {
 		if (!isset($this->locks[$id])) {
 			if (DEBUG > 0) {
-				throw new Exception("user $userid not locked");
+				throw new Exception("user $id not locked");
 			}
 			return false;
 		}
 
-		$lockId = CACHE_IDENTIFIER.'clusterLockUtilHandle'.urlencode($id);
-		$this->cacheUtil->write($lockId,false,1,CacheUtility::CM_MEMCACHE);
-		unset ($this->locks[$id]);
+		$lockId = CACHE_IDENTIFIER . 'clusterLockUtilHandle' . urlencode($id);
+		$this->cacheUtil->write($lockId, false, 1, $this->forceType);
+		unset($this->locks[$id]);
 		return true;
 	}
 
 	function __destruct() {
 		if (count($this->locks) > 0) {
 			if (DEBUG > 0) {
-				throw new Exception("these locks have not been unlocked:".print_r($this->locks, true));
+				throw new Exception("these locks have not been unlocked:" . print_r($this->locks, true));
 			}
 		}
 	}
